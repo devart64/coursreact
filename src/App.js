5809e44014693affd9c3e125b4db6d2bc82c06d1
Stephen Dupré <stephendupre64@gmail.com>
2020-12-22 09:00:18 +0100

    démarage du premier cours

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items:  [
+        "Buy milk",
+        "Go to the dentist"
+      ]
+    }
+  }
+  render() {
+    return(
+      <div className="container mt-3">
+        
+        <div className="row">
+          <div className="col-md-6 offset-3">
+        <h1>Shopping List</h1>
+          <ul>
+            { this.state.items.map(item => {
+                return(<li>{item}</li>)
+              })
+            }
+          </ul>
+          </div>
+        </div>      
+      </div>
+    );
+  }
 }
 
 export default App;
